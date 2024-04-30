e6be5505d721fdadfed68360c4a7fedea03e51fc
0xaplus <codewithaplus@gmail.com>
2024-04-30 02:15:52 +0100

    feat: upload and Grade

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,30 +1,50 @@
 const Uploader = require("../services/upload2Cloud");
+const { grade } = require("./grade.controller");
 
 const bucketName = process.env.BUCKET_NAME || "abdulsalam";
+const answersFolder = "C:/Users/DELL/Downloads/answer_sheets";
 
 // Upload single file
-exports.upload = async (req, res) => {
+exports.uploadAndGrade = async (req, res) => {
   try {
     // Check if a file was uploaded
-    if (!req.file) {
+    if (!req.files) {
       return res.status(400).json({ error: "No file uploaded." });
     }
 
-    const filePath = req.file.path
+    let urls = new Map();
 
-    // Upload the file to Google Cloud Storage
-    await Uploader.googleCloudUploader(bucketName, filePath, "testupload3.txt");
+    const files = Object.values(req.files); // Convert to array
+    await Promise.all(
+      files.map(async (element) => {
+        const filePath = element[0].path;
+        const filename = element[0].fieldname;
+        console.log("filePath", filePath);
+        const url = await Uploader.googleCloudUploader(
+          bucketName,
+          filePath,
+          `testupload-${filename + Date.now()}.txt`
+        );
+        urls.set(filename, url);
+      })
+    );
 
+    const questionUrl = urls.get("file1");
+    const guideUrl = urls.get("file2");
+
+    //Issue here
+    console.log(questionUrl);
+    const result = await grade(questionUrl, guideUrl);
+
+    console.log(result);
     res
       .status(200)
       .json({ message: "File uploaded to Google Cloud Storage successfully." });
   } catch (error) {
     console.error("Error uploading file to Google Cloud Storage:", error);
-    res
-      .status(500)
-      .json({
-        error:
-          "An error occurred while uploading the file to Google Cloud Storage.",
-      });
+    res.status(500).json({
+      error:
+        "An error occurred while uploading the file to Google Cloud Storage.",
+    });
   }
 };
