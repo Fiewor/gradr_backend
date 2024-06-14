5ba1117e9b0e8b1f32d6ef0e97961eaacc1bef7a
Fiewor <johnfiewor@gmail.com>
2024-06-14 17:23:54 +0100

    feat: improve prompt for getting online answers

--- services/getOnlineAnswers.js
+++ services/getOnlineAnswers.js
@@ -3,7 +3,10 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLEAI_API_KEY);
 const textModel = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 async function getOnlineAnswers(question) {
-  const prompt = `Find the right answers to the question: ${question}. Provide links to references where possible. Ignore any mention of Question number and/or marks/points.`;
+  const prompt = `You're a helpful search engine. 
+  Find the right answers to the question: ${question}. 
+  Provide links to references where possible. 
+  Ignore any mention of Question number and/or marks/points.`;
 
   const result = await textModel.generateContent(prompt);
   const response = result.response;
