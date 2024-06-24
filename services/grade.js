d83a8585120e7d40b121446ea863a385628e0a37
Fiewor <johnfiewor@gmail.com>
2024-06-24 17:35:57 +0100

    feat: add model parameters, handle pdf files, get file extension from arguments

--- services/grade.js
+++ services/grade.js
@@ -4,24 +4,37 @@ const getOnlineAnswers = require("./getOnlineAnswers");
 const downloadFilesToLocal = require("./downloadFilesToLocal");
 const readFolderContent = require("./readFolderContent");
 const { formulateGradingPrompt } = require("./formulateGradingPrompt");
+const { GoogleAIFileManager } = require("@google/generative-ai/files");
+const { splitConvertRead } = require("./splitConvertRead");
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLEAI_API_KEY);
+const textModel = genAI.getGenerativeModel({
+  model: "gemini-pro",
+  generationConfig: {
+    temperature: 0,
+    topP: 0.5,
+    topK: 1,
+  },
+});
+
+// const fileManager = new GoogleAIFileManager(process.env.GOOGLEAI_API_KEY);
 
 async function grade(
   questionUrl,
   guideUrl,
   marksAttainable = 0,
   dependencyLevel = 0,
-  extraPrompt = ""
+  extraPrompt = "",
+  questionExtension = ".pdf",
+  guideExtension = ".pdf"
 ) {
-  const genAI = new GoogleGenerativeAI(process.env.GOOGLEAI_API_KEY);
-  const textModel = genAI.getGenerativeModel({ model: "gemini-pro" });
-
   let questionPath = "",
     guidePath = "";
 
   const fetchQuestion = await downloadFilesToLocal(
     questionUrl,
     "files",
-    "question.docx",
+    `question${questionExtension}`,
     "document"
   );
   if (fetchQuestion?.status === "success") {
@@ -31,9 +44,10 @@ async function grade(
   const fetchGuide = await downloadFilesToLocal(
     guideUrl,
     "files",
-    "guide.docx",
+    `guide${guideExtension}`,
     "document"
   );
+
   if (fetchGuide?.status === "success") {
     guidePath = fetchGuide?.path;
   }
@@ -41,59 +55,79 @@ async function grade(
   let question = "",
     guide = "";
 
-  const extractor = new WordExtractor();
+  console.log("questionPath: ", questionPath);
+
   if (questionPath) {
-    const questionData = await extractor.extract(questionPath);
-    question = questionData.getBody();
+    if (questionExtension === ".docx") {
+      const extractor = new WordExtractor();
+      const questionData = await extractor.extract(questionPath);
+      question = questionData.getBody();
+    } else {
+      // question is a .pdf, read using AI instead of with word extractor
+      question = await splitConvertRead(questionPath);
+    }
   }
 
   if (guidePath) {
-    const guideData = await extractor.extract(guidePath);
-    guide = guideData.getBody();
+    if (guideExtension === ".docx") {
+      const guideData = await extractor.extract(guidePath);
+      guide = guideData.getBody();
+    } else {
+      guide = await splitConvertRead(guidePath);
+    }
   }
 
+  console.log("question: ", question);
+
   const onlineAnswers = await getOnlineAnswers(question);
+  console.log("onlineAnswers: ", onlineAnswers);
+
   const folderContent = await readFolderContent();
 
   if (folderContent?.error || !folderContent?.length) {
     return { status: "error", message: "No student answer to grade." };
   }
 
-  const responses = await Promise.allSettled(
-    // go through all the student's answers
-    folderContent.map(async ({ text: studentAnswer }) => {
-      if (question && guide && onlineAnswers) {
-        // generate prompt
-        const gradingPrompt = formulateGradingPrompt(
-          question,
-          guide,
-          studentAnswer,
-          onlineAnswers,
-          marksAttainable,
-          dependencyLevel,
-          extraPrompt
-        );
+  setTimeout(async () => {
+    const responses = await Promise.allSettled(
+      // go through all the student's answers
+      folderContent.map(async ({ text: studentAnswer }) => {
+        if (question && guide && onlineAnswers) {
+          // generate prompt
+          const gradingPrompt = formulateGradingPrompt(
+            question,
+            guide,
+            studentAnswer,
+            onlineAnswers,
+            marksAttainable,
+            dependencyLevel,
+            extraPrompt
+          );
 
-        // grade
-        const result = await textModel.generateContent(gradingPrompt);
-        const response = result.response;
-        console.log("output of grading: ", response.text());
-        return response.text();
-      } else {
-        return { status: "error", message: "One or more parameters are falsy" };
-      }
-    })
-  );
+          // grade
+          const result = await textModel.generateContent(gradingPrompt);
+          const response = result.response;
+          console.log("output of grading: ", response.text());
+          return response.text();
+        } else {
+          return {
+            status: "error",
+            message: "One or more parameters are falsy",
+          };
+        }
+      })
+    );
 
-  const data = {
-    question,
-    markingGuide: guide,
-    gradingResponse: responses,
-    onlineAnswers: onlineAnswers,
-    answerFilesUrls: folderContent.map(({ url }) => url),
-  };
+    const data = {
+      question,
+      markingGuide: guide,
+      gradingResponse: responses,
+      onlineAnswers: onlineAnswers,
+      answerFilesUrls: folderContent.map(({ url }) => url),
+    };
 
-  return { status: "success", data };
+    return { status: "success", data };
+  }, 1000);
 }
 
 module.exports = {
