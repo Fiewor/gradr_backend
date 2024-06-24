9c7ee291298c0e23427e1151ddab3a423ea1cf31
Fiewor <johnfiewor@gmail.com>
2024-06-24 17:36:50 +0100

    refactor: change to expect base64 string instead of file path

--- services/fileToGenerativePart.js
+++ services/fileToGenerativePart.js
@@ -1,10 +1,9 @@
-const fs = require("fs");
-
 // Converts local file information to a GoogleGenerativeAI.Part object.
-function fileToGenerativePart(path, type, mimeType) {
+function fileToGenerativePart(path, type, mimeType = "image/png") {
   return {
     inlineData: {
-      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      // was using `Buffer.from(fs.readFileSync(path)).toString("base64")` when passing file path of images, but since i'm passing base64 for pdf, decided to pass base64 for images too
+      data: path, //cause i'm passing the base64string directly.
       mimeType,
     },
   };
